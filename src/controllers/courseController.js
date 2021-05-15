120ea48e197452ab3cedfce2947731d628558672
@Sinha199 <anshu.resource@gmail.com>
2021-05-15 11:36:52 +0530

    added controller for all routes

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -45,6 +45,39 @@ const addCourse = async(req,res) =>{
         res.send({error});
     }
 };
+
+const editCourse = async(req,res) =>{
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name' , 'description' , 'author' , 'link'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if(!isValidOperation)
+    {
+        res.code(400).send({
+            error:'Not a valid operation'
+        });
+        return;
+    }
+
+    try {
+       const updatedCourse = await client.update({
+           table='courses',
+           records:[
+               {
+                id: req.params.id,
+                name: req.body.name,
+                description: req.body.description,
+                author: req.body.author,
+                link: req.body.link
+               }
+           ]
+       });
+       res.send({updatedCourse}); 
+    } catch (error) {
+        res.send({error});
+    }
+};
+
 module.exports = {
     getCourses,
     getSpecificCourses,
